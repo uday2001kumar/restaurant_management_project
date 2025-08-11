d8c46667f02434c0375dfd39d3e9f4d878ef66b2
uday2001kumar <challagiriudaykumar@gmail.com>
2025-08-12 00:02:25 +0530

    24202

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.conf import settings
 # Create your views here.
 def home(request):
     return render(request, 'home.html',{'restaurent_name':settings.RESTAURENT_NAME})
+
+def about(request):
+    return render(request,'about.html')
